6775295e2e0cc397bb94480315ec7945b23dadef
josuehoenicka <josuee.vzla@gmail.com>
2023-10-17 08:58:42 -0300

    new: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UI';
+  readonly APIUrl = "http://localhost:5176/api/toDoList/"
+
+  constructor(private http: HttpClient){
+
+  }
+
+  notes: any = [];
+
+  refreshNotes(){
+    this.http.get(this.APIUrl + 'GetNotes').subscribe(data => {
+      this.notes = data;
+    })
+  }
+
+  ngOnInit(): void {
+    this.refreshNotes();
+  }
+
+  addNotes(){
+    var newNotes = (<HTMLInputElement>document.getElementById("newNotes")).value;
+    var formData = new FormData();
+    formData.append("newNotes", newNotes);
+    this.http.post(this.APIUrl + 'AddNotes', formData).subscribe(data => {
+      alert(data);
+      this.refreshNotes();
+    })
+  }
+
+  deleteNotes(id: any){
+    this.http.delete(this.APIUrl + 'DeleteNotes?id=' + id).subscribe(data => {
+      alert(data);
+      this.refreshNotes();
+    })
+  }
 }
